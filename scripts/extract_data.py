66c9cf6b6f83ccbe9c0216087ef8e251f2a9de58
Petar Pavlović <p.petar.pavlovic.96@gmail.com>
2025-12-19 00:25:38 +0100

    extract_data.py now keeps track of request made per minute and waits when it would go over the limit

--- scripts/extract_data.py
+++ scripts/extract_data.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 import asyncio
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 from pathlib import Path
 from openaq import AsyncOpenAQ
 
 API_KEY = "28167e861b5b17b41ef6f9e4ab183790ae37b8df75697bbb95212edde98e215d"
 COUNTRIES = [132, 110, 103, 80, 75, 65, 131, 62, 74, 97, 104] #ids of all the balkan countries
+
+API_LIMIT_PER_MINUTE = 60
+API_LIMIT_REMAINING = API_LIMIT_PER_MINUTE
+MINUTE = 0
 
 def save_data(df, output_dir="data/raw", prefix="", suffix=""):
     if df.empty:
@@ -23,70 +27,65 @@ def save_data(df, output_dir="data/raw", prefix="", suffix=""):
     
     return parquet_file
 
-async def fetch_and_save_data(output_dir, func, *args, **kwargs):
+async def fetch_and_save_data(output_dir, async_func, *args, **kwargs):
+    global API_LIMIT_REMAINING 
+    global MINUTE
+
     results = True
     page_num = 0
 
     kwargs['limit'] = 1000
     
     while results:
-        page_num += 1
-        kwargs['page'] = page_num
-        response1 = func(*args, **kwargs)
+        while API_LIMIT_REMAINING < 5:
+            minutes = datetime.now().minute
+            if minutes > MINUTE or (minutes == 0 and MINUTE != 0):
+                MINUTE = datetime.now().minute
+                API_LIMIT_REMAINING = API_LIMIT_PER_MINUTE
+                break
+
+            await asyncio.sleep(60)
 
         page_num += 1
         kwargs['page'] = page_num
-        response2 = func(*args, **kwargs)
+        response1 = async_func(*args, **kwargs)
 
         page_num += 1
         kwargs['page'] = page_num
-        response3 = func(*args, **kwargs)
+        response2 = async_func(*args, **kwargs)
 
         page_num += 1
         kwargs['page'] = page_num
-        response4 = func(*args, **kwargs)
+        response3 = async_func(*args, **kwargs)
 
         page_num += 1
         kwargs['page'] = page_num
-        response5 = func(*args, **kwargs)
+        response4 = async_func(*args, **kwargs)
 
-        responses = await asyncio.gather(response1, response2, response3, response4, response5)
+        page_num += 1
+        kwargs['page'] = page_num
+        response5 = async_func(*args, **kwargs)
+       
+        minutes = datetime.now().minute
+        if minutes > MINUTE or (minutes == 0 and MINUTE != 0):
+            MINUTE = datetime.now().minute
+            API_LIMIT_REMAINING = API_LIMIT_PER_MINUTE
 
-        if len(responses[0].results) == 0:
-            results = False
-        else:
-            data = responses[0].dict()
-            df = pd.json_normalize(data['results'])
-            save_data(df, output_dir)
+        API_LIMIT_REMAINING -= 5
 
-        if len(responses[1].results) == 0:
-            results = False
-        else:
-            data = responses[1].dict()
-            df = pd.json_normalize(data['results'])
-            save_data(df, output_dir)
+        tasks = [response1, response2, response3, response4, response5]
+        
+        async for task in asyncio.as_completed(tasks):
+            response = task.result() 
 
-        if len(responses[2].results) == 0:
-            results = False
-        else:
-            data = responses[2].dict()
-            df = pd.json_normalize(data['results'])
-            save_data(df, output_dir)
+            if len(response.results) == 0:
+                results = False
+            else:
+                data = response.dict()
+                df = pd.json_normalize(data['results'])
+                save_data(df, output_dir)
 
-        if len(responses[3].results) == 0:
-            results = False
-        else:
-            data = responses[3].dict()
-            df = pd.json_normalize(data['results'])
-            save_data(df, output_dir)
-
-        if len(responses[4].results) == 0:
-            results = False
-        else:
-            data = responses[4].dict()
-            df = pd.json_normalize(data['results'])
-            save_data(df, output_dir)
-
+        
 async def main():
     client = AsyncOpenAQ(api_key=API_KEY)
     save_path = "data/raw/"
