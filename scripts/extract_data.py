b870168a455faf2bdafd47bde5540b9fce3e695c
Petar Pavlović <p.petar.pavlovic.96@gmail.com>
2025-12-18 23:05:02 +0100

    Implemented batch requests

--- scripts/extract_data.py
+++ scripts/extract_data.py
@@ -25,21 +25,67 @@ def save_data(df, output_dir="data/raw", prefix="", suffix=""):
 
 async def fetch_and_save_data(output_dir, func, *args, **kwargs):
     results = True
-    page_num = 1
+    page_num = 0
 
     kwargs['limit'] = 1000
     
     while results:
+        page_num += 1
         kwargs['page'] = page_num
-        response = await func(*args, **kwargs)
-        
-        if len(response.results) == 0:
+        response1 = func(*args, **kwargs)
+
+        page_num += 1
+        kwargs['page'] = page_num
+        response2 = func(*args, **kwargs)
+
+        page_num += 1
+        kwargs['page'] = page_num
+        response3 = func(*args, **kwargs)
+
+        page_num += 1
+        kwargs['page'] = page_num
+        response4 = func(*args, **kwargs)
+
+        page_num += 1
+        kwargs['page'] = page_num
+        response5 = func(*args, **kwargs)
+
+        responses = await asyncio.gather(response1, response2, response3, response4, response5)
+
+        if len(responses[0].results) == 0:
             results = False
         else:
-            data = response.dict()
+            data = responses[0].dict()
             df = pd.json_normalize(data['results'])
             save_data(df, output_dir)
-            page_num +=1
+
+        if len(responses[1].results) == 0:
+            results = False
+        else:
+            data = responses[1].dict()
+            df = pd.json_normalize(data['results'])
+            save_data(df, output_dir)
+
+        if len(responses[2].results) == 0:
+            results = False
+        else:
+            data = responses[2].dict()
+            df = pd.json_normalize(data['results'])
+            save_data(df, output_dir)
+
+        if len(responses[3].results) == 0:
+            results = False
+        else:
+            data = responses[3].dict()
+            df = pd.json_normalize(data['results'])
+            save_data(df, output_dir)
+
+        if len(responses[4].results) == 0:
+            results = False
+        else:
+            data = responses[4].dict()
+            df = pd.json_normalize(data['results'])
+            save_data(df, output_dir)
 
 async def main():
     client = AsyncOpenAQ(api_key=API_KEY)
