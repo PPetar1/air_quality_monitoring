21efd9fa4b72c30c96a2064898c50bf00d8fc0d1
Petar Pavlović <p.petar.pavlovic.96@gmail.com>
2025-12-20 21:26:30 +0100

    scripts/extract_data.py now extracts location, country, parameter, sensor, measurements for all Balkan countries when run
    Extraction is with this mostly done, only logging and documentation are
    TODO, so we can move to scheduling with airflow and transformations with
    dbt.

--- scripts/extract_data.py
+++ scripts/extract_data.py
@@ -8,16 +8,21 @@ from datetime import datetime, timedelta
 from pathlib import Path
 import os
 import logging
+import sys
 
 logging.getLogger("transport").setLevel(logging.CRITICAL)
 
 SCRIPT_NAME = os.path.basename(__file__)
 API_KEY = "28167e861b5b17b41ef6f9e4ab183790ae37b8df75697bbb95212edde98e215d"
-COUNTRIES = [132, 110, 103, 80, 75, 65, 131, 62, 74, 97, 104] #ids of all the balkan countries
+COUNTRY_IDS = [132, 110, 103, 80, 75, 65, 131, 62, 74, 97, 104] #ids of all the balkan countries
 BATCH_SIZE = 5
+LIMIT = 1000
 MAX_RETRY = 3
 API_LIMIT_PER_MINUTE = 60
 SAVE_PATH = "data/raw/"
+LOCATION_IDS = []
+SENSOR_IDS = []
+DATETIME_FROM = datetime.now() - timedelta(days=1)
 
 class RateLimiter:
     def __init__(self, max_per_minute=API_LIMIT_PER_MINUTE):
@@ -56,12 +61,31 @@ def save_data(df, output_dir="data/raw", prefix="", suffix=""):
     
     return parquet_file
 
-async def fetch_and_save_data(output_dir, async_func, batch_size=1, *args, **kwargs):
+async def fetch_data_and_save(output_dir, async_func, *args, **kwargs):
+    await RATE_LIMITER.acquire()
+    
+    try:
+        response = await execute_with_retry(async_func, *args, **kwargs) 
+    except Exception as err:
+        sys.stderr.write("A task has failed " + str(MAX_RETRY + 1) + " times. Error message: " + str(err) + "\n")
+        return
+
+    RATE_LIMITER.release()
+
+    data = response.dict()
+    df = pd.json_normalize(data['results'])
+
+    if 'locations_id' in kwargs:
+        df['location_id'] = kwargs['locations_id']
+
+    
+    save_data(df, output_dir)
+
+
+async def fetch_paged_data_and_save(output_dir, async_func, batch_size=1, *args, **kwargs):
     results = True
     page_num = 0
 
-    kwargs['limit'] = 1000
-    
     while results:
         await RATE_LIMITER.acquire(batch_size)
         
@@ -72,11 +96,14 @@ async def fetch_and_save_data(output_dir, async_func, batch_size=1, *args, **kwa
 
             tasks.append(execute_with_retry(async_func, *args, **kwargs))
         
+        error_count = 0
+
         async for task in asyncio.as_completed(tasks):
             try:
                 response = task.result() 
             except Exception as err:
-                print("A task has failed " + str(MAX_RETRY + 1) + " times. Error message: " + str(err))
+                sys.stderr.write("A task has failed " + str(MAX_RETRY + 1) + " times. Error message: " + str(err) + "\n")
+                error_count += 1
                 continue
                 
             RATE_LIMITER.release()
@@ -86,7 +113,14 @@ async def fetch_and_save_data(output_dir, async_func, batch_size=1, *args, **kwa
             else:
                 data = response.dict()
                 df = pd.json_normalize(data['results'])
+            
+                if 'sensors_id' in kwargs:
+                    df['sensor_id'] = kwargs['sensors_id']
+            
                 save_data(df, output_dir)
+
+        if error_count == batch_size:
+            break
 
 async def execute_with_retry(async_func, max_retry=MAX_RETRY, *args, **kwargs):
     retry_count = 0
@@ -101,7 +135,12 @@ async def execute_with_retry(async_func, max_retry=MAX_RETRY, *args, **kwargs):
                 retry_count += 1
             else:
                 raise err
-    
+   
+async def fetch_and_save_ids(api_call, id_list, *args, **kwargs):
+    response = await api_call(*args, **kwargs)
+    id_list.extend(map(lambda x: x.id, response.results))
+    return response
+
 async def main():
     try:
         start_time = datetime.now()
@@ -110,13 +149,25 @@ async def main():
         client = AsyncOpenAQ(api_key=API_KEY)
         
         tasks = []
-        for _ in range(10):
-            tasks.append(fetch_and_save_data(SAVE_PATH + "locations", client.locations.list, batch_size=BATCH_SIZE, countries_id = COUNTRIES))
+        tasks.append(fetch_paged_data_and_save(SAVE_PATH + "location", fetch_and_save_ids, api_call=client.locations.list, id_list=LOCATION_IDS, batch_size=BATCH_SIZE, countries_id=COUNTRY_IDS))
+        tasks.append(fetch_paged_data_and_save(SAVE_PATH + "country", client.countries.list, batch_size=BATCH_SIZE))
+        tasks.append(fetch_paged_data_and_save(SAVE_PATH + "parameters", client.parameters.list, batch_size=BATCH_SIZE))
+     
+        await asyncio.gather(*tasks)
 
-        for _ in range(5):
-            tasks.append(fetch_and_save_data(SAVE_PATH + "locations", client.locations.list, countries_id = COUNTRIES))
+
+        tasks = []
+        for locations_id in LOCATION_IDS:
+            tasks.append(fetch_data_and_save(SAVE_PATH + "sensor", fetch_and_save_ids, api_call=client.locations.sensors, id_list=SENSOR_IDS, locations_id=locations_id))
 
         await asyncio.gather(*tasks)
+
+        tasks = []
+        for sensors_id in SENSOR_IDS:
+            tasks.append(fetch_paged_data_and_save(SAVE_PATH + "measurement", client.measurements.list, sensors_id=sensors_id, datetime_from=DATETIME_FROM))
+
+        await asyncio.gather(*tasks)
+
 
         await client.close()
 
@@ -125,13 +176,12 @@ async def main():
     
     except Exception as err:
         end_time = datetime.now()
-        print("CRITICAL\nScript " + SCRIPT_NAME + " encountered an unrecoverable error at: " + end_time.strftime("%H:%M:%S") +  "\nError message: ||| " + str(err) + " |||\nExecution time: " + str((end_time - start_time).total_seconds()) + "s")
+        sys.stderr.write("CRITICAL\nScript " + SCRIPT_NAME + " encountered an unrecoverable error at: " + end_time.strftime("%H:%M:%S") +  
+              "\nError message: ||| " + str(err) + " |||\nExecution time: " + str((end_time - start_time).total_seconds()) + "s")
 
 if __name__ == "__main__":
     asyncio.run(main())
 
 # TODO: 
-#       saving location ids (and later sensor ids) to use for other extraction, 
-#       extraction of all data,
 #       logging,
 #       documentation
