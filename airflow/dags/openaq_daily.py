8d7479893400ce471314f20f18b43e136058e216
Petar Pavlović <p.petar.pavlovic.96@gmail.com>
2025-12-25 19:29:47 +0100

    openaq_daily.py fix

--- airflow/dags/openaq_daily.py
+++ airflow/dags/openaq_daily.py
@@ -11,27 +11,36 @@ import duckdb
 
 from airflow.sdk import dag, task
 
+API_LIMIT_PER_MINUTE = 60
+
+API_KEY = "28167e861b5b17b41ef6f9e4ab183790ae37b8df75697bbb95212edde98e215d"
+CLIENT = AsyncOpenAQ(api_key=API_KEY)
+
+COUNTRY_IDS = [132, 110, 103, 80, 75, 65, 131, 62, 74, 97, 104] #ids of all the balkan countries
+BATCH_SIZE = 5
+LIMIT = 1000
+MAX_RETRY = 3
+SAVE_PATH = "data/raw/"
 
 class RateLimiter:
-    def __init__(self, max_per_minute):
-        self.semaphore = aiologic.Semaphore(max_per_minute)
-        self.list = [0]*max_per_minute
-        self.lock = aiologic.Lock()
+    semaphore = aiologic.Semaphore(API_LIMIT_PER_MINUTE)
+    list = [0]*API_LIMIT_PER_MINUTE
+    lock = aiologic.Lock()
 
-    async def acquire(self, amount=1):
-        async with self.lock:
+    async def acquire(amount=1):
+        async with RateLimiter.lock:
             for _ in range(amount):
-                await self.semaphore.async_acquire()
-                last_request = self.list.pop(0)
+                await RateLimiter.semaphore.async_acquire()
+                last_request = RateLimiter.list.pop(0)
 
             time_to_wait = last_request + 60 - time.time()
             if time_to_wait > 0:
                 await asyncio.sleep(time_to_wait)
 
-    def release(self):
-        self.list.append(time.time())
-        self.semaphore.release()
-
+    def release():
+        RateLimiter.list.append(time.time())
+        RateLimiter.semaphore.release()
+    
 def save_data(df, output_dir, prefix="", suffix=""):
     if df.empty:
         return None
@@ -54,16 +63,16 @@ def save_data(df, output_dir, prefix="", suffix=""):
     
     return parquet_file
 
-async def fetch_data_and_save(output_dir, job_name, rate_limiter, max_retry, async_func, *args, **kwargs):
-    await rate_limiter.acquire()
+async def fetch_data_and_save(output_dir, job_name, async_func, *args, **kwargs):
+    await RateLimiter.acquire()
     
     try:
-        response = await execute_with_retry(async_func, rate_limiter, max_retry, *args, **kwargs) 
+        response = await execute_with_retry(async_func, *args, **kwargs) 
     except Exception as err:
-        sys.stderr.write("A task has failed " + str(max_retry + 1) + " times. Error message: " + str(err) + "\n") #TODO: Logging
+        sys.stderr.write("A task has failed " + str(MAX_RETRY + 1) + " times. Error message: " + str(err) + "\n") #TODO: Logging
         return
 
-    rate_limiter.release()
+    RateLimiter.release()
 
     data = response.dict()
     df = pd.json_normalize(data['results'])
@@ -77,19 +86,19 @@ async def fetch_data_and_save(output_dir, job_name, rate_limiter, max_retry, asy
     save_data(df, output_dir, prefix=job_name)
 
 
-async def fetch_paged_data_and_save(output_dir, job_name, rate_limiter, max_retry, async_func, batch_size=1, *args, **kwargs):
+async def fetch_paged_data_and_save(output_dir, job_name, async_func, batch_size=1, *args, **kwargs):
     results = True
     page_num = 0
 
     while results:
-        await rate_limiter.acquire(batch_size)
+        await RateLimiter.acquire(batch_size)
         
         tasks = []
         for _ in range(batch_size):
             page_num += 1
             kwargs['page'] = page_num
 
-            tasks.append(execute_with_retry(async_func, rate_limiter, max_retry, *args, **kwargs))
+            tasks.append(execute_with_retry(async_func, *args, **kwargs))
         
         error_count = 0
 
@@ -97,11 +106,11 @@ async def fetch_paged_data_and_save(output_dir, job_name, rate_limiter, max_retr
             try:
                 response = task.result() 
             except Exception as err:
-                sys.stderr.write("A task has failed " + str(max_retry + 1) + " times. Error message: " + str(err) + "\n") #TODO: Logging
+                sys.stderr.write("A task has failed " + str(MAX_RETRY + 1) + " times. Error message: " + str(err) + "\n") #TODO: Logging
                 error_count += 1
                 continue
                 
-            rate_limiter.release()
+            RateLimiter.release()
 
             if len(response.results) == 0:
                 results = False
@@ -120,16 +129,16 @@ async def fetch_paged_data_and_save(output_dir, job_name, rate_limiter, max_retr
         if error_count == batch_size:
             break
 
-async def execute_with_retry(async_func, rate_limiter, max_retry, *args, **kwargs):
+async def execute_with_retry(async_func, *args, **kwargs):
     retry_count = 0
     while True:
         try:
             if retry_count != 0:
-                await rate_limiter.acquire()
+                await RateLimiter.acquire()
             return await async_func(*args, **kwargs)
         except Exception as err:
-            rate_limiter.release()
-            if retry_count < max_retry:
+            RateLimiter.release()
+            if retry_count < MAX_RETRY:
                 retry_count += 1
             else:
                 raise err
@@ -142,49 +151,32 @@ async def fetch_and_save_ids(api_call, id_list, *args, **kwargs):
 @dag(
     dag_id="openaq_daily",
     schedule="@daily",
-    start_date=pendulum.datetime(2025, 12, 10, tz="UTC"),
     catchup=False,
     description="Daily run of openaq pipeline, fetching data from the api and then ingesting it into bronze, silver and gold.",
     tags=["openaq"],
 )
 def openaq_daily():
-    API_LIMIT_PER_MINUTE = 60
-    RATE_LIMITER = RateLimiter(API_LIMIT_PER_MINUTE)
-    
-    API_KEY = "28167e861b5b17b41ef6f9e4ab183790ae37b8df75697bbb95212edde98e215d"
-    CLIENT = AsyncOpenAQ(api_key=API_KEY)
-    
-    @task(multiple_outputs=True)    
-    def initial_setup():
-        COUNTRY_IDS = [132, 110, 103, 80, 75, 65, 131, 62, 74, 97, 104] #ids of all the balkan countries
-        BATCH_SIZE = 5
-        LIMIT = 1000
-        MAX_RETRY = 3
-        SAVE_PATH = "data/raw/"
-
-        return {"COUNTRY_IDS": COUNTRY_IDS, "BATCH_SIZE": BATCH_SIZE, "LIMIT": LIMIT, "MAX_RETRY": MAX_RETRY, "SAVE_PATH": SAVE_PATH}
-
     @task()
-    def fetch_location(SAVE_PATH,  MAX_RETRY, BATCH_SIZE, COUNTRY_IDS,CLIENT=CLIENT, RATE_LIMITER=RATE_LIMITER):
+    def fetch_location():
         location_ids = []
-        asyncio.run(fetch_paged_data_and_save(SAVE_PATH + "location/new", "location", RATE_LIMITER, MAX_RETRY, fetch_and_save_ids, api_call=CLIENT.locations.list, id_list=location_ids, batch_size=BATCH_SIZE, countries_id=COUNTRY_IDS))
+        asyncio.run(fetch_paged_data_and_save(SAVE_PATH + "location/new", "location", fetch_and_save_ids, api_call=CLIENT.locations.list, id_list=location_ids, batch_size=BATCH_SIZE, countries_id=COUNTRY_IDS))
         return location_ids
 
     @task()
-    def fetch_parameter(SAVE_PATH,  MAX_RETRY, BATCH_SIZE,CLIENT=CLIENT, RATE_LIMITER=RATE_LIMITER):
-        asyncio.run(fetch_paged_data_and_save(SAVE_PATH + "country/new", "country", RATE_LIMITER, MAX_RETRY, CLIENT.countries.list, batch_size=BATCH_SIZE))
+    def fetch_parameter():
+        asyncio.run(fetch_paged_data_and_save(SAVE_PATH + "country/new", "country", CLIENT.countries.list, batch_size=BATCH_SIZE))
 
     @task()
-    def fetch_country(SAVE_PATH,  MAX_RETRY, BATCH_SIZE,CLIENT=CLIENT, RATE_LIMITER=RATE_LIMITER):
-        asyncio.run(fetch_paged_data_and_save(SAVE_PATH + "parameter/new", "parameter", RATE_LIMITER, MAX_RETRY, CLIENT.parameters.list, batch_size=BATCH_SIZE))
+    def fetch_country():
+        asyncio.run(fetch_paged_data_and_save(SAVE_PATH + "parameter/new", "parameter", CLIENT.parameters.list, batch_size=BATCH_SIZE))
 
     @task()
-    def fetch_sensor(SAVE_PATH,  MAX_RETRY, BATCH_SIZE, location_ids,CLIENT=CLIENT, RATE_LIMITER=RATE_LIMITER):
+    def fetch_sensor(location_ids):
         sensor_ids = []
 
         tasks = []
         for locations_id in location_ids:
-            tasks.append(fetch_data_and_save(SAVE_PATH + "sensor/new", "sensor", RATE_LIMITER, MAX_RETRY, fetch_and_save_ids, api_call=CLIENT.locations.sensors, id_list=sensor_ids, locations_id=locations_id))
+            tasks.append(fetch_data_and_save(SAVE_PATH + "sensor/new", "sensor", fetch_and_save_ids, api_call=CLIENT.locations.sensors, id_list=sensor_ids, locations_id=locations_id))
 
         loop = asyncio.get_event_loop()
         loop.run_until_complete(asyncio.gather(*tasks))
@@ -192,29 +184,30 @@ def openaq_daily():
         return sensor_ids
 
     @task()
-    def fetch_measurement(SAVE_PATH,  MAX_RETRY, BATCH_SIZE, sensor_ids,CLIENT=CLIENT, RATE_LIMITER=RATE_LIMITER):
+    def fetch_measurement(sensor_ids):
+        datetime_to = pendulum.now('UTC')
+        
         last_extraction_timestamp_file_path = Path(SAVE_PATH + "last_extraction_timestamp.txt")
         if Path.exists(last_extraction_timestamp_file_path):
             with open(last_extraction_timestamp_file_path, "r") as file:
                 datetime_from = pendulum.parse(file.read())
         else:
-            datetime_from = {{ start_date }}
+            datetime_from = datetime_to.subtract(days=7)
 
-        datetime_to = pendulum.now('UTC')
 
         tasks = []
         for sensors_id in sensor_ids:
-            tasks.append(fetch_paged_data_and_save(SAVE_PATH + "measurement/new", "measurement", RATE_LIMITER, MAX_RETRY, CLIENT.measurements.list, sensors_id=sensors_id, datetime_from=datetime_from, datetime_to=datetime_to))
+            tasks.append(fetch_paged_data_and_save(SAVE_PATH + "measurement/new", "measurement", CLIENT.measurements.list, sensors_id=sensors_id, datetime_from=datetime_from, datetime_to=datetime_to))
 
         loop = asyncio.get_event_loop()
         loop.run_until_complete(asyncio.gather(*tasks))
 
         with open(last_extraction_timestamp_file_path, "w") as file:
-            file.write(datetime_to)
+            file.write(datetime_to.to_atom_string())
 
     @task()
     def close_connection():
-        asyncio.run(client.close())
+        asyncio.run(CLIENT.close())
 
     @task()
     def create_dwh():
@@ -229,18 +222,17 @@ def openaq_daily():
 
     @task.bash()
     def dbt_run():
-        return "cd ../dbt && dbt run"
+        return "cd $AIRFLOW_HOME/../dbt && dbt run"
 
     @task.bash()
     def archive_files():
-        return "cd ../data/raw && mv location/new/* location/ && mv parameter/new/* parameter/ && mv country/new/* country/ && mv sensor/new/* sensor/ && mv measurement/new/* measurement/"
+        return "cd $AIRFLOW_HOME/../data/raw && mv location/new/* location/ && mv parameter/new/* parameter/ && mv country/new/* country/ && mv sensor/new/* sensor/ && mv measurement/new/* measurement/"
 
-    global_vars = initial_setup()
-    location_ids = fetch_location(global_vars["SAVE_PATH"], global_vars["MAX_RETRY"], global_vars["BATCH_SIZE"], global_vars["COUNTRY_IDS"])
-    fetch_parameter = fetch_parameter(global_vars["SAVE_PATH"], global_vars["MAX_RETRY"], global_vars["BATCH_SIZE"])
-    fetch_country = fetch_country(global_vars["SAVE_PATH"], global_vars["MAX_RETRY"], global_vars["BATCH_SIZE"])
-    sensor_ids = fetch_sensor(global_vars["SAVE_PATH"], global_vars["MAX_RETRY"], global_vars["BATCH_SIZE"], location_ids)
-    fetch_measurement = fetch_measurement(global_vars["SAVE_PATH"], global_vars["MAX_RETRY"], global_vars["BATCH_SIZE"], sensor_ids)
+    location_ids = fetch_location()
+    fetch_parameter = fetch_parameter()
+    fetch_country = fetch_country()
+    sensor_ids = fetch_sensor(location_ids)
+    fetch_measurement = fetch_measurement(sensor_ids)
     close_connection = close_connection()
 
     create_dwh = create_dwh()
